bc0d9ffb792a60bc8b79beaad42f8854a09ce443
Dongyoung_Kim <521ab@naver.com>
2017-09-05 20:37:41 +0900

    .

--- LimitSwitch/StartupTask.py
+++ LimitSwitch/StartupTask.py
@@ -1,1 +1,21 @@
 # Add your code here to run in your startup task
+import RPi.GPIO as gpio
+import time
+
+gpio.setmode(gpio.BCM)
+gpio.setup(24,IN)   # Limit Switch
+gpio.setup(7,OUT)   # MotorA1
+gpio.setup(8,OUT)   # MotorA2
+
+
+while True:
+    if not(gpio.input(24)):
+        gpio.output(7,True)
+        gpio.output(8,False)
+    elif gpio.input(24):
+        gpio.output(7,False)
+        gpio.output(8,True)
+        time.sleep(2)
+    else:
+        gpio.output(7,False)
+        gpio.output(8,False)
